5e29e475363203d1a8310521748be83862e91d32
ExpiredSaury <318129549@qq.com>
2023-04-03 13:49:59 +0800

    axf

---
+++ axf/cart/cartserializer.py
@@ -0,0 +1,31 @@
+
+from rest_framework import serializers
+
+from home.models import AxfGoods, AxfCart
+from market.marketserializers import GoodsSerializer
+
+
+class CartAddSerializer(serializers.Serializer):
+    goodsid = serializers.CharField(required=True)
+    token = serializers.CharField()
+    def validate_goodsid(self, value):
+        value = int(value)
+        print(value)
+        the_goods = AxfGoods.objects.filter(pk=value).first()
+        if not the_goods:
+            raise serializers.ValidationError("商品不存在")
+        return value
+
+    def validate_token(self, data):
+        if not data:
+            raise serializers.ValidationError("token不存在")
+        return data
+
+
+
+
+class CartSerializer(serializers.ModelSerializer):
+    c_goods = GoodsSerializer()  # 关联序列化
+    class Meta:
+        model = AxfCart
+        fields = "__all__"
